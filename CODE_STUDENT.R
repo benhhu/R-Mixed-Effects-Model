b2872470e788fc6a9e1ee24f46399f5678a9bc0f
Hui Hu <benhhu@users.noreply.github.com>
2016-10-12 13:26:55 -0400

    Add files via upload

--- CODE_STUDENT.R
+++ CODE_STUDENT.R
@@ -1,4 +1,4 @@
-setwd('//file.phhp.ufl.edu/home/huihu/files/MixedEffectModel/') #change the path
+setwd('//file.phhp.ufl.edu/home/huihu/files/MixedEffectsModel') #change the path
 
 #########################################
 #install the lme4 and arm packages first#
@@ -48,7 +48,7 @@ m5<-lmer(sbp~bmi+age+bmi:age+(1+bmi|id),data=dfsbp)
 display(m5)
 
 #5.2.5 non-nested models
-m6<-lmer(sbp~(1|id)+(1|pm),data=dfsbp)
+m6<-lmer(sbp~(1|id)+(1|am),data=dfsbp)
 display(m6)
 
 ##############################################################
@@ -78,51 +78,3 @@ exp(fixef(m8)[2]+c(-1.96,1.96)*se.fixef(m8)[2])
 m9<-glmer(ex~bmi+(1|id),family=poisson(link="log"),data=dfsbp)
 display(m9)
 head(coef(m9)[[1]])
-
-#5.4.6 JAGS
-#install.packages('rjags')  
-#Note: Make sure you also install the JAGS
-library('rjags')
-dfsbp<-read.csv("sbp.csv",header=T)
-#prepare the data to import into JAGS
-y<-dfsbp$sbp
-x<-dfsbp$bmi
-id<-dfsbp$id
-N<-nrow(dfsbp) #the total number of measurements
-K<-length(unique(id)) #the total number of groups
-#count the number of measurements for each group
-dfsbp$count=1
-nk<-aggregate(count~id,data=dfsbp,sum)$count
-
-#sets up model object
-jags<-jags.model('JAGS.bug',data=list('y'=y,'x'=x,'N'=N,'K'=K,'id'=id),n.chains=4,n.adapt=100)
-#burn-in period
-update(jags,10000)
-#draw 1,0000 samples from the sampler
-k<-jags.samples(jags,c('gamma','b','precy','preca','a'),10000)
-
-#A function to plot the posterior distribution of the parameter of interest
-inference=function(num,nome){
-  k1=k[[num]][]
-  plot(NA,NA,xlim=c(0,10000),ylim=range(k1))
-  for (i in 1:4) lines(1:10000,k[[num]][1,,i],col=i)
-  k2=density(k1)
-  plot(k2,type='l',xlab='',main=nome)
-  z=quantile(k1,c(0.025,0.975))
-  abline(v=z,col='red',lty=3)
-  print(c(mean(k1),z))
-}
-#inference on the parameters of interest
-par(mfrow=c(2,2))
-inference(3,'gamma')
-inference(2,'b')
-par(mfrow=c(1,1))
-
-par(mfrow=c(2,2))
-inference(5,'precy')
-inference(4,'preca')
-par(mfrow=c(1,1))
-
-#intercept for the 51st group
-mean(k$a[51,,])
-quantile(k$a[51,,],c(0.025,0.975))
